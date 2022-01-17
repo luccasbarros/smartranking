75d2a553dafd4ffa975c4c6d876cefc58ca24716
Luccas Barros <ldbcandido@gmail.com>
2022-01-17 17:00:58 -0300

    feat: added nest  pipes e validators

--- src/jogadores/dtos/create-player.dto.ts
+++ src/jogadores/dtos/create-player.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreatePlayerDTO {
+  @IsNotEmpty()
   readonly phoneNumber: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly name: string;
 }
--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreatePlayerDTO } from './dtos/create-player.dto';
 import { IPlayer } from './interfaces/jogador.interface';
 import { PlayersService } from './players.service';
@@ -8,6 +17,7 @@ export class JogadoresController {
   constructor(private readonly playersService: PlayersService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   async createUpdatePlayer(@Body() createPlayerDto: CreatePlayerDTO) {
     await this.playersService.createUpdatePlayer(createPlayerDto);
   }
