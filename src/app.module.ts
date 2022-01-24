879676b05bb6c2e7391763c9c641b908e10d74a9
Luccas Barros <ldbcandido@gmail.com>
2022-01-24 12:19:48 -0300

    feat: added mongoose schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       'mongodb+srv://mydb123:M1dogcp9@cluster0.mdw2d.mongodb.net/smartranking?retryWrites=true&w=majority',
     ),
     JogadoresModule,
+    CategoriesModule,
   ],
   controllers: [],
   providers: [],
